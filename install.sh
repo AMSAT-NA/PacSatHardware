85a6a7ab75be378551aa36135ac682df0e95f6fc
Chris Thompson <g0kla@arrl.net>
2024-06-28 11:45:40 -0400

    Add hcg files for Launchpad and Blinky

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INSTALL_DIR=../PacSatSW
+
+if [ $# -lt 2 ]; then
+    echo "Usage: install.sh [launchpad|blinky] <install-dir>"
+    exit 1
+fi
+
+FOLDER=$1
+INSTALL_DIR=$2
+
+if [ ! -d $FOLDER ]; then
+    echo "$FOLDER is not a valid folder"
+    exit 1
+fi
+
+if [ ! -d $INSTALL_DIR ]; then
+    echo "$INSTALL_DIR is not a valid install destination"
+    exit 1
+fi
+
+rm -r $INSTALL_DIR/hcg 
+rm -r $INSTALL_DIR/hcgExtras 
+
+echo Installing $FOLDER/hcg files in $INSTALL_DIR
+cp -r $FOLDER/hcg $INSTALL_DIR || exit 1
+echo Installing $FOLDER/hcgExtra files in $INSTALL_DIR
+cp -r $FOLDER/hcgExtras $INSTALL_DIR || exit 1
