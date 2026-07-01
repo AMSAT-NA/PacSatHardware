ef8e3112fd388aacaf5fa3ee48b751300729c1f1
Corey Minyard <corey@minyard.net>
2026-06-30 21:08:22 -0700

    Fix SPI5 configuration
    Signed-off-by: Corey Minyard <corey@minyard.net>

--- afsk3/hcg/source/spi.c
+++ afsk3/hcg/source/spi.c
@@ -452,9 +452,9 @@ void spiInit(void)
                   | (uint32)((uint32)0U << 21U)  /* wait on enable */
                   | (uint32)((uint32)0U << 20U)  /* shift direction */
                   | (uint32)((uint32)0U << 17U)  /* clock polarity */
-                  | (uint32)((uint32)0U << 16U)  /* clock phase */
-                  | (uint32)((uint32)79U << 8U) /* baudrate prescale */
-                  | (uint32)((uint32)16U << 0U);  /* data word length */
+                  | (uint32)((uint32)1U << 16U)  /* clock phase */
+                  | (uint32)((uint32)39U << 8U) /* baudrate prescale */
+                  | (uint32)((uint32)8U << 0U);  /* data word length */
 
     /** - Data Format 1 */
     spiREG5->FMT1 = (uint32)((uint32)0U << 24U)  /* wdelay */
@@ -463,9 +463,9 @@ void spiInit(void)
                   | (uint32)((uint32)0U << 21U)  /* wait on enable */
                   | (uint32)((uint32)0U << 20U)  /* shift direction */
                   | (uint32)((uint32)0U << 17U)  /* clock polarity */
-                  | (uint32)((uint32)0U << 16U)  /* clock phase */
+                  | (uint32)((uint32)1U << 16U)  /* clock phase */
                   | (uint32)((uint32)79U << 8U) /* baudrate prescale */
-                  | (uint32)((uint32)16U << 0U);  /* data word length */
+                  | (uint32)((uint32)8U << 0U);  /* data word length */
 
     /** - Data Format 2 */
     spiREG5->FMT2 = (uint32)((uint32)0U << 24U)  /* wdelay */
@@ -505,7 +505,7 @@ void spiInit(void)
     /** - enable interrupts */
     spiREG5->INT0 = (spiREG5->INT0 & 0xFFFF0000U)
                   | (uint32)((uint32)0U << 9U)  /* TXINT */
-                  | (uint32)((uint32)0U << 8U)  /* RXINT */
+                  | (uint32)((uint32)1U << 8U)  /* RXINT */
                   | (uint32)((uint32)0U << 6U)  /* OVRNINT */
                   | (uint32)((uint32)0U << 4U)  /* BITERR */
                   | (uint32)((uint32)0U << 3U)  /* DESYNC */
@@ -602,15 +602,15 @@ void spiInit(void)
                     | (uint32)((uint32)0U << 2U)  /* SCS[2] */
                     | (uint32)((uint32)0U << 3U)  /* SCS[3] */
                     | (uint32)((uint32)0U << 8U)  /* ENA */
-                    | (uint32)((uint32)0U << 9U)  /* CLK */
-                    | (uint32)((uint32)0U << 10U)  /* SIMO[0] */
-                    | (uint32)((uint32)0U << 11U)  /* SOMI[0] */
-                    | (uint32)((uint32)0U << 17U)  /* SIMO[1] */
-                    | (uint32)((uint32)0U << 18U)  /* SIMO[2] */
-                    | (uint32)((uint32)0U << 19U)  /* SIMO[3] */
-                    | (uint32)((uint32)0U << 25U)  /* SOMI[1] */
-                    | (uint32)((uint32)0U << 26U)  /* SOMI[2] */
-                    | (uint32)((uint32)0U << 27U); /* SOMI[3] */
+                    | (uint32)((uint32)1U << 9U)  /* CLK */
+                    | (uint32)((uint32)1U << 10U)  /* SIMO[0] */
+                    | (uint32)((uint32)1U << 11U)  /* SOMI[0] */
+                    | (uint32)((uint32)1U << 17U)  /* SIMO[1] */
+                    | (uint32)((uint32)1U << 18U)  /* SIMO[2] */
+                    | (uint32)((uint32)1U << 19U)  /* SIMO[3] */
+                    | (uint32)((uint32)1U << 25U)  /* SOMI[1] */
+                    | (uint32)((uint32)1U << 26U)  /* SOMI[2] */
+                    | (uint32)((uint32)1U << 27U); /* SOMI[3] */
 
     /** - Initialize TX and RX data buffer Status */
     g_spiPacket_t[4U].tx_data_status  = SPI_READY;
