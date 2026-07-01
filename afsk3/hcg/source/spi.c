2c0d6884ec957889657d464437c96dfa70c4a946
Corey Minyard <corey@minyard.net>
2026-06-30 21:36:23 -0700

    Yet more spi5 fixes
    Signed-off-by: Corey Minyard <corey@minyard.net>

--- afsk3/hcg/source/spi.c
+++ afsk3/hcg/source/spi.c
@@ -539,10 +539,10 @@ void spiInit(void)
                     | (uint32)((uint32)1U << 8U)  /* ENA */
                     | (uint32)((uint32)1U << 9U)  /* CLK */
                     | (uint32)((uint32)1U << 10U)  /* SIMO[0] */
-                    | (uint32)((uint32)1U << 11U)  /* SOMI[0] */
-                    | (uint32)((uint32)0U << 17U)  /* SIMO[1] */
-                    | (uint32)((uint32)0U << 18U)  /* SIMO[2] */
-                    | (uint32)((uint32)0U << 19U)  /* SIMO[3] */
+                    | (uint32)((uint32)0U << 11U)  /* SOMI[0] */
+                    | (uint32)((uint32)1U << 17U)  /* SIMO[1] */
+                    | (uint32)((uint32)1U << 18U)  /* SIMO[2] */
+                    | (uint32)((uint32)1U << 19U)  /* SIMO[3] */
                     | (uint32)((uint32)0U << 25U)  /* SOMI[1] */
                     | (uint32)((uint32)0U << 26U)  /* SOMI[2] */
                     | (uint32)((uint32)0U << 27U); /* SOMI[3] */
